f46a8a7eeb167813894fc23028dc841d33ea4a82
eddyim <edwardim@calvisitor-10-105-150-31.calvisitor.1918.berkeley.edu>
2016-11-29 23:41:57 -0800

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
 
- 
+	 var navBarPosition = $(".navbar-container").offset().top;
+	 $(window).scroll(function() {
+	  
+	  var currentScroll = $(window).scrollTop();
+	  
+	  console.log(currentScroll);
+	  if (currentScroll < navBarPosition) {
+	    $(".navbar-container").removeClass("nav-bar-fixed");
+	  } else {
+	    $(".navbar-container").addClass("nav-bar-fixed");
+	  }
+
+	});
+
+	 $(".form-button").click(function(){
+	 	$("#modal").fadeIn();
+	 });
+
+	 $("#modal").click(function(){
+	 	$("#modal").fadeOut();
+	 })
+	 $(function() {
+ 		 $('a[href*="#"]:not([href="#"])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     		 var target = $(this.hash);
+    		 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     		 if (target.length) {
+       			 $('html, body').animate({
+      			    scrollTop: target.offset().top
+    			    }, 1000);
+    	    return false;
+      }
+    }
+  });
+});
 });
